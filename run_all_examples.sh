4b6e926cc25a27cdc5861b25a135364c15662f24
Matt Wilson <matthew.wilson.synesis@gmail.com>
2024-10-18 08:54:22 +1100

    + added examples

---
+++ run_all_examples.sh
@@ -0,0 +1,130 @@
+#! /bin/bash
+
+ScriptPath=$0
+Dir=$(cd $(dirname "$ScriptPath"); pwd)
+Basename=$(basename "$ScriptPath")
+CMakeDir=${SIS_CMAKE_BUILD_DIR:-$Dir/_build}
+MakeCmd=${SIS_CMAKE_COMMAND:-make}
+
+ListOnly=0
+RunMake=1
+
+
+# ##########################################################
+# command-line handling
+
+while [[ $# -gt 0 ]]; do
+
+  case $1 in
+    -l|--list-only)
+
+      ListOnly=1
+      ;;
+    -M|--no-make)
+
+      RunMake=0
+      ;;
+    --help)
+
+      cat << EOF
+BDUT, is a very simple - simplistic, in fact - small, header-only, standalone library for C and C++. Its intent is to be bundled into other projects for which it is not desired to couple to a more sophisticated library.
+Copyright (c) 2020-2024, Matthew Wilson and Synesis Information Systems
+Runs all example programs
+
+$ScriptPath [ ... flags/options ... ]
+
+Flags/options:
+
+    behaviour:
+
+    -l
+    --list-only
+        lists the target programs but does not execute them
+
+    -M
+    --no-make
+        does not execute CMake and make before running tests
+
+
+    standard flags:
+
+    --help
+        displays this help and terminates
+
+EOF
+
+      exit 0
+      ;;
+    *)
+
+      >&2 echo "$ScriptPath: unrecognised argument '$1'; use --help for usage"
+
+      exit 1
+      ;;
+  esac
+
+  shift
+done
+
+
+# ##########################################################
+# main()
+
+status=0
+
+if [ $RunMake -ne 0 ]; then
+
+  if [ $ListOnly -eq 0 ]; then
+
+    echo "Executing build (via command \`$MakeCmd\`) and then running all example programs"
+
+    mkdir -p $CMakeDir || exit 1
+
+    cd $CMakeDir
+
+    $MakeCmd
+    status=$?
+
+    cd ->/dev/null
+  fi
+else
+
+  if [ ! -d "$CMakeDir" ] || [ ! -f "$CMakeDir/CMakeCache.txt" ] || [ ! -d "$CMakeDir/CMakeFiles" ]; then
+
+    >&2 echo "$ScriptPath: cannot run in '--no-make' mode without a previous successful build step"
+  fi
+fi
+
+if [ $status -eq 0 ]; then
+
+  if [ $ListOnly -ne 0 ]; then
+
+    echo "Listing all example programs"
+  else
+
+    echo "Running all example programs"
+  fi
+
+  for f in $(find $CMakeDir -type f '(' -name 'example*' ')' -exec test -x {} \; -print)
+  do
+
+    if [ $ListOnly -ne 0 ]; then
+
+      echo "would execute $f:"
+
+      continue
+    fi
+
+    echo
+    echo "executing $f:"
+
+    # NOTE: we do not break on fail, because, this being a unit-testing library, the scratch-tests actually fail
+    $f
+  done
+fi
+
+exit $status
+
+
+# ############################## end of file ############################# #
+
